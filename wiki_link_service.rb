01c4f94332001a52493d3f5031ba3b5b01950312
Brett Terpstra <brett.terpstra@oracle.com>
2025-02-24 04:28:52 -0600

    allow multiple links in service
    @improved Allow multiple matches in service

--- wiki_link_service.rb
+++ wiki_link_service.rb
@@ -7,11 +7,10 @@
 # extension.
 
 BOOKMARKER = "~/scripts/bookmarker"
+DUTI = "/opt/homebrew/bin/duti"
 
 def parse_input(input)
-  if input =~ /\[\[([a-z0-9 ]+)(?:\|.*?)?\]\]/i
-    Regexp.last_match(1)
-  end
+  input.scan(/\[\[([a-z0-9 ]+)(?:\|.*?)?\]\]/i).flatten
 end
 
 def notify(message)
@@ -22,29 +21,37 @@ def yn(message)
   `osascript -e 'button returned of (display dialog "#{message}" with title "Wiki Link Service" buttons {"Yes", "No"})'`.strip == "Yes"
 end
 
-first_id = parse_input($stdin.read)
+ids = parse_input($stdin.read)
 
-if first_id.nil?
+if ids.empty?
   notify "No WikiLink found"
   exit 1
 end
 
-path = `#{BOOKMARKER} find "#{first_id}"`.strip
+ids.each do |id|
+  path = `#{BOOKMARKER} find "#{id}"`.strip
 
-if path.empty?
-  notify "No bookmark found for #{first_id}"
-  exit
+  if path.empty?
+    # If bookmark isn't found, try to find it with Spotlight
+    spotlight_path = `mdfind "description:#{id}"`.strip.split(/\n/).first
+    path = spotlight_path if File.exist?(spotlight_path)
+
+    if path.empty?
+      notify "No bookmark found for #{first_id}"
+      exit
+    end
+  end
+
+  if File.directory?(path)
+    `open -R "#{path}"` if yn("Open #{File.basename(path)} in Finder?")
+    exit 0
+  end
+
+  app = "default application"
+
+  if File.executable?(DUTI)
+    app = `#{DUTI} -x #{File.extname(path).delete(".")}`.split(/\n/).first.sub(/\.app$/, "")
+  end
+
+  `open "#{path}"` if yn("Open #{File.basename(path)} in #{app}?")
 end
-
-if File.directory?(path)
-  `open -R "#{path}"`
-  exit
-end
-
-app = "default application"
-
-if File.executable?("/opt/homebrew/bin/duti")
-  app = `/opt/homebrew/bin/duti -x #{File.extname(path).delete(".")}`.split(/\n/).first.sub(/\.app$/, "")
-end
-
-`open "#{path}"` if yn("Open #{File.basename(path)} in #{app}?")
